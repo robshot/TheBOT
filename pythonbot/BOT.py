a5be09625d8f80a507b71d30af0af5b36f4dd418
Michiel <Michiel@MSI>
2017-06-14 19:36:30 +0200

    Preparation for VS

--- pythonbot/BOT.py
+++ pythonbot/BOT.py
@@ -1,63 +1,19 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
+from irc import *
+import os
+import random
 
-import socket
+channel = "#pythonbottest"
+server = "178.118.237.238"
+port = 5000
+nickname = "TheBOT"
 
-#SERVER = 'irc.freenode.net'
-SERVER = 'irc.quakenet.org'
-#SERVER = '178.118.237.238'
-#PORT = 5000
-PORT = 6667
-CHANNEL = "#pythonbottest2"
-BOTNICK = 'TheBOT2'
-COMMAND_SIGN = '@'
+irc = IRC()
+irc.connect(server, port, channel, nickname)
 
-def Send(target, message):
-	sock.send(('PRIVMSG '+target+' :'+message+'\r\n').encode())
-#	print(message)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Connecting to server')
-sock.connect((SERVER,PORT))
-#sock.send(('quote PASS TheBOT:Test').encode())
-sock.send(('USER '+BOTNICK+' '+BOTNICK+' '+BOTNICK+" :hi.\r\n").encode())
-sock.send(('NICK '+BOTNICK + "\r\n").encode())
-#sock.send(('JOIN '+CHANNEL + "\r\n").encode())
-#sock.send(('PRIVMSG '+CHANNEL+" :This is a test.\r\n").encode())
-print('Server connection succeeded!')
+while 1:
+    text = irc.get_text()
+    print text
 
-def main():
-	print('Main loop')
-	import time
-	time.sleep(5)
-	sock.send(('JOIN ' + CHANNEL + "\r\n").encode()) 
-	while True:
-		print('Main loop while')
-#		sock.send('Testje')
-#		Send(CHANNEL,'Test')
-		received = sock.recv(2048).decode('latin1')
-		print(received)
-#		received = received[:len(received)-len('\r\n')]
-#		print(received)
-#		Send(CHANNEL,'Testje')
-		import time
-		time.sleep(5)		
-#		print('Listening...')
-#   		text = sock.recv(2048).decode('UTF-8')
-#   		print(text)   #print text to console
-
-#	   	if text.find('PING') != -1:                          #check if 'PING' is found
-#      			sock.send(('PONG ' + text.split() [1] + '\r\n').encode()) #returnes 'PONG' back to the server (prevents pinging out!)	
-			
-#Shenanigans.
-				
-#		if received.find(COMMAND_SIGN+'test') != -1:
-#			try:
-#				Send(CHANNEL,'@'+received.split(CHANNEL,1)[1].split(' ')[1].replace(':','')+' Yes, I am working. You idiot...')
-#				import time
-#				time.sleep(5)
-#			except:
-#				continue					
-#if __name__ == "__main__":
-#	main()
-
+    if "PRIVMSG" in text and channel in text and "hello" in text:
+        irc.send(channel, "Hello!")
--- pythonbot/irc.py
+++ pythonbot/irc.py
@@ -2,27 +2,25 @@ import socket
 import sys
 
 class IRC:
+    irc = socket.socket()
 
-  irc = socket.socket()
-  
-  def __init__(self):  
-    self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-    def send(self, chan, msg):
-        self.irc.send("PRIVMSG " + chan + " " + msg + "n")
- 
-    def connect(self, server, port, channel, botnick):
-        #defines the socket
-        print "connecting to:"+server
-        self.irc.connect((server, port))
-        self.irc.send("USER " + botnick + " " + botnick +" " + botnick + " :This is a fun bot!n")
-        self.irc.send("NICK " + botnick + "n")               
-        self.irc.send("JOIN " + channel + "n")
- 
-    def get_text(self):
-        text=self.irc.recv(2040)
- 
-        if text.find('PING') != -1:                      
-            self.irc.send('PONG ' + text.split() [1] + 'rn') 
- 
-        return text
+    def __init__(self):
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, chan, msg):
+        self.irc.send("PRIVMSG " + chan + " " + msg + "n")
+
+    def connect(self, server, port, channel, botnick):
+        print "connecting to:"+server
+        self.irc.connect((server, port))
+        self.irc.send("USER " + botnick + " " + botnick +" " + botnick + " :This is a fun bot!n")
+        self.irc.send("NICK " + botnick + "n")
+        self.irc.send("JOIN " + channel + "n")
+
+    def get_text(self):
+        text=self.irc.recv(2040)
+
+        if text.find('PING') != -1:
+            self.irc.send('PONG ' + text.split() [1] + 'rn') 
+
+        return text
