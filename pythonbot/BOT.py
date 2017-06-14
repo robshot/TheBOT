d16687ac55dd3c1e3691a16a1c840787da26991d
robshot <robbie.demaegdt@hotmail.com>
2017-06-14 11:54:59 +0000

    Quakenet

--- pythonbot/BOT.py
+++ pythonbot/BOT.py
@@ -3,11 +3,12 @@
 import socket
 
 #SERVER = 'irc.freenode.net'
-#SERVER = 'irc.quakenet.org'
-SERVER = '178.118.237.238'
-PORT = 5000
-CHANNEL = "#pythonbottest"
-BOTNICK = 'TheBOT'
+SERVER = 'irc.quakenet.org'
+#SERVER = '178.118.237.238'
+#PORT = 5000
+PORT = 6667
+CHANNEL = "#pythonbottest2"
+BOTNICK = 'TheBOT2'
 COMMAND_SIGN = '@'
 
 def Send(target, message):
@@ -17,29 +18,35 @@ def Send(target, message):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Connecting to server')
 sock.connect((SERVER,PORT))
-sock.send(('quote PASS TheBOT:Test').encode())
+#sock.send(('quote PASS TheBOT:Test').encode())
 sock.send(('USER '+BOTNICK+' '+BOTNICK+' '+BOTNICK+" :hi.\r\n").encode())
 sock.send(('NICK '+BOTNICK + "\r\n").encode())
-sock.send(('JOIN '+CHANNEL + "\r\n").encode())
-sock.send(('PRIVMSG '+CHANNEL+" :This is a test.\r\n").encode())
+#sock.send(('JOIN '+CHANNEL + "\r\n").encode())
+#sock.send(('PRIVMSG '+CHANNEL+" :This is a test.\r\n").encode())
 print('Server connection succeeded!')
 
 def main():
 	print('Main loop')
+	import time
+	time.sleep(5)
+	sock.send(('JOIN ' + CHANNEL + "\r\n").encode()) 
 	while True:
+		print('Main loop while')
+#		sock.send('Testje')
 #		Send(CHANNEL,'Test')
-#		received = sock.recv(2048).decode('latin1')
+		received = sock.recv(2048).decode('latin1')
+		print(received)
 #		received = received[:len(received)-len('\r\n')]
 #		print(received)
 #		Send(CHANNEL,'Testje')
-#		import time
-#		time.sleep(5)		
+		import time
+		time.sleep(5)		
 #		print('Listening...')
-   		text = sock.recv(2048).decode('UTF-8')
-   		print(text)   #print text to console
+#   		text = sock.recv(2048).decode('UTF-8')
+#   		print(text)   #print text to console
 
-	   	if text.find('PING') != -1:                          #check if 'PING' is found
-      			sock.send(('PONG ' + text.split() [1] + '\r\n').encode()) #returnes 'PONG' back to the server (prevents pinging out!)	
+#	   	if text.find('PING') != -1:                          #check if 'PING' is found
+#      			sock.send(('PONG ' + text.split() [1] + '\r\n').encode()) #returnes 'PONG' back to the server (prevents pinging out!)	
 			
 #Shenanigans.
 				
